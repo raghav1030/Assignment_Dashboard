ab42db5aeed8a77a9223d0604cee4db2fd6ea74d
RAGHAV <94848646+raghav1030@users.noreply.github.com>
2024-01-10 15:29:04 +0530

    Modified bg color of some components

--- src/components/payments/PaymentHeader.jsx
+++ src/components/payments/PaymentHeader.jsx
@@ -7,13 +7,13 @@ const PaymentHeader = ({timeline , setTimeline}) => {
 
 
     return (
-        <div className='flex flex-col gap-6 bg-[#FFF]   h-full w-full text-black'>
+        <div className='flex flex-col gap-6    h-full w-full text-black'>
             <div className='flex justify-between items-center w-full h-[2.25rem]'>
                 <div className='text-[1.25rem] font-[500] leading-7  '>
                     Overview
                 </div>
 
-                <div className='w-[8.5625rem] p-[0.6rem]  flex items-center justify-between flex-shrink-0 rounded-[0.25rem] border-[#D9D9D9] border-[1px] border-solid h-8  ' >
+                <div className='w-[8.5625rem] p-[0.6rem] bg-[#FFF]  flex items-center justify-between flex-shrink-0 rounded-[0.25rem] border-[#D9D9D9] border-[1px] border-solid h-8  ' >
 
                     <select onChange={(e) => setTimeline(e.target.value)} className='text-[#4D4D4D] bg text-[1rem] w-full font-normal leading-[1.5rem] outline-none transition-all ease-in-out duration-500 appearance-none  ' name="timeline" id="timeline">
                         <option value="Last Week" selected>Last Week</option>
@@ -28,7 +28,7 @@ const PaymentHeader = ({timeline , setTimeline}) => {
                 </div>
             </div>
 
-            <div className='flex justify-between self-stretch gap-6 w-full items-center  '>
+            <div className='flex justify-between self-stretch gap-6 w-full items-center bg-[#FFF]  '>
                 <div className='p-6 w-1/2    flex flex-col gap-4  items-start justify-center  shadow-[0px_2px_6px_0px] shadow-[#1a181e0a] rounded-[0.5rem] '>
                     <p className='text-[1rem] font-normal leading-6 text-[#4D4D4D] '>
                         Online Orders
